d79cc584b805109cc877d1e70fe64cdd5d3e1646
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-24 14:01:42 -0600

    Add project files.

---
+++ Find-The-Smallest-Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_The_Smallest_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
