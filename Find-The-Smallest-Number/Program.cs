0cf49d13c12185c44c772d087f9717e0e4cfddd2
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-24 14:11:44 -0600

    add basic program logic

--- Find-The-Smallest-Number/Program.cs
+++ Find-The-Smallest-Number/Program.cs
@@ -10,7 +10,36 @@ namespace Find_The_Smallest_Number
     {
         static void Main(string[] args)
         {
+            int[] numArr = new int[0];
+            int minNum;
+            int num;
+            int counter;
 
+            Console.WriteLine("Please enter the number of integers that you'd like to compare: ");
+            counter = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < counter; i++)
+            {
+                Console.WriteLine("Enter integer: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                numArr.Append(num);
+                Console.WriteLine(numArr);
+            }
+
+            minNum = numArr[0];
+
+            for (int i = 0; i < numArr.Length; i++)
+            {
+                if (numArr[i] < minNum)
+                {
+                    minNum = numArr[i];
+                }
+            }
+
+            Console.WriteLine("The smallest number is " + minNum + ".");
+            Console.WriteLine("Press 'enter' to close the program.");
+            Console.ReadLine();
         }
     }
 }
